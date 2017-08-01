ad90d3de905a4425a250381234fbf00e792ce775
lab113n3\EV <baturaandrew@gmail.com>
2017-08-01 15:34:05 +0300

    исправлены ошибки в jade

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,12 @@
 // функция находится в отдельном файле в папке lib
 var fortune = require('./lib/fortune.js');
 var express = require('express');
-//var hbs = require("hbs");
-// app.engine('handlebars', hbs);
-// устанавливаем путь к каталогу с частичными представлениями
-//hbs.registerPartials(__dirname + "/views/partials");
-var handlebars = require('express-handlebars')
-    .create({
-        layoutsDir: __dirname +
-        '/views/layouts', partialsDir: __dirname + '/views/partials'
-    });
-
 var app = express();
 
-//var jade
-
 app.set('port', process.env.PORT || 3000);
-
-
-//app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-//app.engine('handlebars', handlebars.engine);
-//app.set('view engine', 'handlebars');
-
 app.set('view engine', 'jade');
 //промежуточное ПО static
 app.use(express.static(__dirname + '/public'));
-
-
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
@@ -41,7 +21,6 @@ app.get("/", function (request, response) {
     });
 });
 
-
 app.get("/contact", function (request, response) {
 
     response.render("contact.jade", {
@@ -53,9 +32,8 @@ app.get("/contact", function (request, response) {
 });
 
 app.get("/about", function (request, response) {
-
-    response.render("about.jade", 
-    { title: "Common informaition" });
+    response.render("about.jade",
+        { title: "Common informaition" });
 });
 app.get('/400', function (req, res) {
     res.render('400.jade');
@@ -64,13 +42,14 @@ app.get('/500', function (req, res) {
     res.render('500.jade');
 });
 
-
 app.get('/tours/hood-river', function (req, res) {
-    res.render('tours/hood-river');
+    res.render('tours/hood-river',
+        { title: "Река Худ" });
 });
 
 app.get('/tours/request-group-rate', function (req, res) {
-    res.render('tours/request-group-rate');
+    res.render('tours/request-group-rate',
+        { title: "Групповой тур" });
 });
 
 app.listen(app.get('port'), function () {
